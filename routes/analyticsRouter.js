45e2b128ab87d796a5014bd12b577d5f17c3e690
ARTushar <1605070@ugrad.cse.buet.ac.bd>
2020-11-16 22:29:07 +0600

    add 30 days statistics router of sales and orders count

--- routes/analyticsRouter.js
+++ routes/analyticsRouter.js
@@ -10,134 +10,180 @@ const cors = require('./cors');
 analyticsRouter.route('/totalproducts')
   .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
   .get(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    if(req.query.from && req.query.to){
-      Products.countDocuments({createdAt: {$gte: req.query.from, $lte: req.query.to}})
+    if (req.query.from && req.query.to) {
+      Products.countDocuments({ createdAt: { $gte: req.query.from, $lte: req.query.to } })
         .then(resp => {
-          res.json({count: resp}).status(200);
+          res.json({ count: resp }).status(200);
         }, err => next(err))
         .catch(err => next(err));
-    } else if(req.query.from){
-      Products.countDocuments({createdAt: {$gte: req.query.from}})
+    } else if (req.query.from) {
+      Products.countDocuments({ createdAt: { $gte: req.query.from } })
         .then(resp => {
-          res.json({count: resp}).status(200);
+          res.json({ count: resp }).status(200);
         }, err => next(err))
         .catch(err => next(err));
-    } else if(req.query.to){
-      Products.countDocuments({createdAt: {$lte: req.query.to}})
+    } else if (req.query.to) {
+      Products.countDocuments({ createdAt: { $lte: req.query.to } })
         .then(resp => {
-          res.json({count: resp}).status(200);
+          res.json({ count: resp }).status(200);
         }, err => next(err))
         .catch(err => next(err));
     } else {
       Products.countDocuments({})
         .then(resp => {
-          res.json({count: resp}).status(200);
+          res.json({ count: resp }).status(200);
         }, err => next(err))
         .catch(err => next(err));
-      
     }
   });
 
 analyticsRouter.route('/totalorders')
   .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
   .get(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    if(req.query.from && req.query.to){
-      Orders.countDocuments({createdAt: {$gte: req.query.from, $lte: req.query.to}})
+    if (req.query.from && req.query.to) {
+      Orders.countDocuments({ createdAt: { $gte: req.query.from, $lte: req.query.to } })
         .then(resp => {
-          res.json({count: resp}).status(200);
+          res.json({ count: resp }).status(200);
         }, err => next(err))
         .catch(err => next(err));
-    } else if(req.query.from){
-      Orders.countDocuments({createdAt: {$gte: req.query.from}})
+    } else if (req.query.from) {
+      Orders.countDocuments({ createdAt: { $gte: req.query.from } })
         .then(resp => {
-          res.json({count: resp}).status(200);
+          res.json({ count: resp }).status(200);
         }, err => next(err))
         .catch(err => next(err));
-    } else if(req.query.to){
-      Orders.countDocuments({createdAt: {$lte: req.query.to}})
+    } else if (req.query.to) {
+      Orders.countDocuments({ createdAt: { $lte: req.query.to } })
         .then(resp => {
-          res.json({count: resp}).status(200);
+          res.json({ count: resp }).status(200);
         }, err => next(err))
         .catch(err => next(err));
     } else {
       Orders.countDocuments({})
         .then(resp => {
-          res.json({count: resp}).status(200);
+          res.json({ count: resp }).status(200);
         }, err => next(err))
         .catch(err => next(err));
-      
+
     }
   })
+
+analyticsRouter.route('/lastmonthorders')
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, async (req, res, next) => {
+    let counts = {};
+    let currentTime = Date.now();
+    let prevTime = currentTime - (24 * 3600 * 1000);
+    for (let i = 1; i <= 30; i++) {
+      try {
+        let resp = await Orders.countDocuments({ createdAt: { $gt: prevTime, $lte: currentTime} })
+        counts['entry' + i] = {
+          date: currentTime,
+          value: resp
+        }
+      } catch (err) {
+        return next(err);
+      }
+      currentTime = prevTime;
+      prevTime = currentTime - (24 * 60 * 60 * 1000) * i;
+    }
+    res.json(counts).status(200);
+  })
+
 
 analyticsRouter.route('/totalusers')
   .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
   .get(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    if(req.query.from && req.query.to){
-      Users.countDocuments({createdAt: {$gte: req.query.from, $lte: req.query.to}})
+    if (req.query.from && req.query.to) {
+      Users.countDocuments({ createdAt: { $gte: req.query.from, $lte: req.query.to } })
         .then(resp => {
-          res.json({count: resp}).status(200);
+          res.json({ count: resp }).status(200);
         }, err => next(err))
         .catch(err => next(err));
-    } else if(req.query.from){
-      Users.countDocuments({createdAt: {$gte: req.query.from}})
+    } else if (req.query.from) {
+      Users.countDocuments({ createdAt: { $gte: req.query.from } })
         .then(resp => {
-          res.json({count: resp}).status(200);
+          res.json({ count: resp }).status(200);
         }, err => next(err))
         .catch(err => next(err));
-    } else if(req.query.to){
-      Users.countDocuments({createdAt: {$lte: req.query.to}})
+    } else if (req.query.to) {
+      Users.countDocuments({ createdAt: { $lte: req.query.to } })
         .then(resp => {
-          res.json({count: resp}).status(200);
+          res.json({ count: resp }).status(200);
         }, err => next(err))
         .catch(err => next(err));
     } else {
       Users.countDocuments({})
         .then(resp => {
-          res.json({count: resp}).status(200);
+          res.json({ count: resp }).status(200);
         }, err => next(err))
         .catch(err => next(err));
-      
+
     }
   })
 
 analyticsRouter.route('/totalsalesvalue')
   .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
   .get(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
-    if(req.query.from && req.query.to){
-      Orders.find({createdAt: {$gte: req.query.from, $lte: req.query.to}}, 'subTotalCost')
+    if (req.query.from && req.query.to) {
+      Orders.find({ createdAt: { $gte: req.query.from, $lte: req.query.to } }, 'subTotalCost')
         .then(orders => {
-          const totalCost = orders.reduce(((accumulator, currentValue) => 
+          const totalCost = orders.reduce(((accumulator, currentValue) =>
             accumulator + currentValue.subTotalCost
-            ), 0);
-          res.json({totalCost}).status(200);
+          ), 0);
+          res.json({ totalCost }).status(200);
         }, err => next(err))
         .catch(err => next(err));
-    } else if(req.query.from){
-      Orders.find({createdAt: {$gte: req.query.from}}, 'subTotalCost')
+    } else if (req.query.from) {
+      Orders.find({ createdAt: { $gte: req.query.from } }, 'subTotalCost')
         .then(orders => {
-          const totalCost = orders.reduce(((accumulator, currentValue) => 
+          const totalCost = orders.reduce(((accumulator, currentValue) =>
             accumulator + currentValue.subTotalCost
-            ), 0);
-          res.json({totalCost}).status(200);
+          ), 0);
+          res.json({ totalCost }).status(200);
         }, err => next(err))
         .catch(err => next(err));
-    } else if(req.query.to){
-      Orders.find({createdAt: {$lte: req.query.to}}, 'subTotalCost')
+    } else if (req.query.to) {
+      Orders.find({ createdAt: { $lte: req.query.to } }, 'subTotalCost')
         .then(orders => {
-          const totalCost = orders.reduce(((accumulator, currentValue) => 
+          const totalCost = orders.reduce(((accumulator, currentValue) =>
             accumulator + currentValue.subTotalCost), 0);
-          res.json({totalCost}).status(200);
+          res.json({ totalCost }).status(200);
         }, err => next(err))
         .catch(err => next(err));
     } else {
       Orders.find({})
         .then(orders => {
-          const totalCost = orders.reduce(((accumulator, currentValue) => accumulator + currentValue.subTotalCost), 0);
-          res.json({totalCost}).status(200);
+          const totalCost = orders.reduce(((accumulator, currentValue) =>
+            accumulator + currentValue.subTotalCost), 0);
+          res.json({ totalCost }).status(200);
         }, err => next(err))
         .catch(err => next(err));
     }
   })
 
+  analyticsRouter.route('/lastmonthsalesvalue')
+  .options(cors.corsWithOptions, (req, res) => res.sendStatus(200))
+  .get(cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, async (req, res, next) => {
+    let counts = {};
+    let currentTime = Date.now();
+    let prevTime = currentTime - (24 * 3600 * 1000);
+    for (let i = 1; i <= 30; i++) {
+      try {
+        let orders = await Orders.find({ createdAt: { $gt: prevTime, $lte: currentTime } }, 'subTotalCost')
+        let totalCost = orders.reduce(((accumulator, currentValue) =>
+          accumulator + currentValue.subTotalCost), 0);
+        counts['entry' + i] = {
+          date: currentTime,
+          value: totalCost 
+        }
+      } catch (err) {
+        return next(err);
+      }
+      currentTime = prevTime;
+      prevTime = currentTime - (24 * 60 * 60 * 1000) * i;
+    }
+    res.json(counts).status(200);
+  })
 
 module.exports = analyticsRouter;
